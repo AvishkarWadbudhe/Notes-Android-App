6331624b14d7252dc9422cfeb47e7c8288324494
Avishkar Wadbudhe <avwadbudhe78@gmail.com>
2023-06-14 20:36:23 +0530

    fixed some bugs

--- app/src/main/java/com/project/notesapp/activities/activity/ViewNotesActivity.java
+++ app/src/main/java/com/project/notesapp/activities/activity/ViewNotesActivity.java
@@ -1,9 +1,11 @@
 package com.project.notesapp.activities.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
